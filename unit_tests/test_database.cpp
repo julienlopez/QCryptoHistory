7f1bdfaff1a3962637fa4a9da8ff02f35d037e0f
julien <julien.lopez51@gmail.com>
2023-03-28 21:54:08 +0200

    adding tests for allTransactions()

--- unit_tests/test_database.cpp
+++ unit_tests/test_database.cpp
@@ -9,7 +9,7 @@ using LibCryptoHistory::Database;
 using LibCryptoHistory::Transaction;
 using LibCryptoHistory::Type;
 
-TEST_CASE("Basic transaction workflow")
+TEST_CASE("Basic transaction workflow", "[database]")
 {
     auto db = Database::open();
     REQUIRE(db.has_value());
@@ -30,4 +30,31 @@ TEST_CASE("Basic transaction workflow")
     REQUIRE(db->addTransaction(transaction3).has_value());
 
     CHECK(db->transactions(btc) == std::vector{transaction1, transaction2, transaction3});
+    CHECK(db->allTransactions() == std::vector{transaction1, transaction2, transaction3});
+}
+
+TEST_CASE("allTransactions() for two currencies", "[database]")
+{
+    auto db = Database::open();
+    REQUIRE(db.has_value());
+    REQUIRE(db->allTransactions().has_value());
+    CHECK(db->allTransactions()->empty());
+
+    const std::string btc = "BTC";
+    const Transaction transaction1{btc, Type::Bought, 1'000., 100., 0.1, "timestamp1"};
+    REQUIRE(db->addTransaction(transaction1).has_value());
+    const Transaction transaction2{btc, Type::Sold, 500., 100., 0.1, "timestamp2"};
+    REQUIRE(db->addTransaction(transaction2).has_value());
+    const Transaction transaction3{btc, Type::Bought, 2'000., 100., 0.1, "timestamp3"};
+    REQUIRE(db->addTransaction(transaction3).has_value());
+
+    const std::string eth = "ETH";
+    const Transaction transaction4{eth, Type::Bought, 1'000., 100., 0.1, "timestamp4"};
+    REQUIRE(db->addTransaction(transaction4).has_value());
+    const Transaction transaction5{eth, Type::Sold, 500., 100., 0.1, "timestamp5"};
+    REQUIRE(db->addTransaction(transaction5).has_value());
+
+    CHECK(db->transactions(btc) == std::vector{transaction1, transaction2, transaction3});
+    CHECK(db->transactions(eth) == std::vector{transaction4, transaction5});
+    CHECK(db->allTransactions() == std::vector{transaction1, transaction2, transaction3, transaction4, transaction5});
 }
